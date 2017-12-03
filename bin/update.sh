edb8b7994ce0d6d4b39f85d1b36f7f660260f1fd
Pierre-Hugues Husson <phh@phh.me>
2017-12-04 00:59:35 +0100

    Initial commit

---
+++ bin/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://f-droid.org/FDroid.apk -O FDroid.apk
+wget https://f-droid.org/repo/com.fsck.k9_23271.apk -O K9Mail.apk
---
+++ foss.mk
@@ -0,0 +1,6 @@
+PRODUCT_COPY_FILES += \
+	vendor/foss/bin/FDroid.apk:system/app/FDroid/FDroid.apk \
+	vendor/foss/bin/K9Mail.apk:system/app/K9Mail/K9Mail.apk
+
+PRODUCT_PACKAGES += \
+	FDroidPrivilegedExtension
